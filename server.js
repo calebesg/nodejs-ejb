d954c8a8d1f7e92c05f3a13fda7c4f9f5f5dccf2
CalebeSG <calebeguimaraes10@gmail.com>
2022-02-21 15:19:42 -0300

    added support static files

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const items = require('./database');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
   response.render('pages/index', { items });
