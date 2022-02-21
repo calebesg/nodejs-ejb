aee4c83a871f8915cf872a6d06d243b407d73d37
CalebeSG <calebeguimaraes10@gmail.com>
2022-02-21 12:53:32 -0300

    created about page

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
+app.get('/about', (request, response) => {
+  response.render('about');
+});
+
 app.listen(8080, console.log('Runing...'));
